8c6c0e9031edaf44a5ebe7672de13f923a54fa65
Markus Wang Halvorsen <markus@halvorsenfamilien.com>
2023-11-04 23:40:33 +0100

    move logdir/valid benches to plotter class

--- scripts/stats/plotters/plotter.py
+++ scripts/stats/plotters/plotter.py
@@ -9,7 +9,28 @@ class Plotter:
     ''' Filename of the output plot '''
     description = 'base plotter class'
     ''' Description of what kind of plot this plotter makes '''
-    
+
+    log_dir = '/cluster/home/markuswh/gem5-runahead/spec2017/logs'
+
+    valid_benchmarks = (
+        "perlbench_s_0",
+        "perlbench_s_1",
+        "perlbench_s_2",
+        "gcc_s_1",
+        "gcc_s_2",
+        "mcf_s_0",
+        "cactuBSSN_s_0",
+        "omnetpp_s_0",
+        "wrf_s_0",
+        "xalancbmk_s_0",
+        "x264_s_0",
+        "x264_s_2",
+        "imagick_s_0",
+        "nab_s_0",
+        "exchange2_s_0",
+        "fotonik3d_s_0",
+    )    
+
     def load_data(self) -> None:
         '''
         Load any data required to produce the plot
--- scripts/stats/plotters/simpoint_weights.py
+++ scripts/stats/plotters/simpoint_weights.py
@@ -12,27 +12,6 @@ class SimPointWeights(Plotter):
     fname = 'sp_weights'
     description = 'SimPoint weights for each benchmark'
 
-    log_dir = '/cluster/home/markuswh/gem5-runahead/spec2017/logs'
-
-    valid_benchmarks = (
-        "perlbench_s_0",
-        "perlbench_s_1",
-        "perlbench_s_2",
-        "gcc_s_1",
-        "gcc_s_2",
-        "mcf_s_0",
-        "cactuBSSN_s_0",
-        "omnetpp_s_0",
-        "wrf_s_0",
-        "xalancbmk_s_0",
-        "x264_s_0",
-        "x264_s_2",
-        "imagick_s_0",
-        "nab_s_0",
-        "exchange2_s_0",
-        "fotonik3d_s_0",
-    )
-
     def load_data(self) -> None:
         self.data = {}
         for bench_dir in os.scandir(self.log_dir):
