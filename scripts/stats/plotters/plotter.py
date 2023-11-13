d90ba1fa7fbb506a544661d9f64b327b6eea0c21
Markus Wang Halvorsen <markus@halvorsenfamilien.com>
2023-11-13 01:44:04 +0100

    some plot refactors

--- scripts/stats/plotters/plotter.py
+++ scripts/stats/plotters/plotter.py
@@ -2,6 +2,13 @@
 Base class for plot producers
 '''
 
+import os
+import json
+import re
+from typing import Any
+import numpy as np
+import distinctipy
+
 class Plotter:
     name = 'base plotter class'
     ''' Pretty name for the figure '''
@@ -11,6 +18,7 @@ class Plotter:
     ''' Description of what kind of plot this plotter makes '''
 
     log_dir = '/cluster/home/markuswh/gem5-runahead/spec2017/logs'
+    stat_file = 'gem5stats.json'
 
     valid_benchmarks = (
         "perlbench_s_0",
@@ -29,7 +37,77 @@ class Plotter:
         "nab_s_0",
         "exchange2_s_0",
         "fotonik3d_s_0",
-    )    
+    )
+
+    def __init__(self):
+        # Pre-compute some highly distinct colors
+        self.color_pool = distinctipy.get_colors(10, n_attempts=20000, rng=1)
+
+    def read_stats(self, bench: str, run_name: str) -> dict:
+        stats_path = os.path.join(self.log_dir, bench, run_name, self.stat_file)
+        with open(stats_path, 'r') as f:
+            data = json.load(f)
+        return data['sections'][0]['stats']
+
+    def benchmarks(self) -> tuple:
+        '''
+        Get a list of all benchmarks in the log directory
+        '''
+        return (d.name for d in os.scandir(self.log_dir) if d.name in self.valid_benchmarks)
+
+    def experiments(self, benchmark: str) -> tuple:
+        '''
+        Get a list of all experiments belonging to a benchmark
+        '''
+        path = os.path.join(self.log_dir, benchmark)
+        return (d.name for d in os.scandir(path))
+
+    def read_stat(self, path: str, val_num: int = 0, data: dict = None) -> Any:
+        '''
+        Read a single value at the given stat path in the given data.
+        '''
+        if data == None:
+            data = self.data
+
+        keys = path.split('.')
+        val = data
+        while len(keys):
+            key = keys[0]
+            val = val[key]
+            keys = keys[1:]
+        val = val['values'][val_num]
+        return val
+
+    def plot_grouped_bars(self, data: dict, plot: Any) -> None:
+        '''
+        Plot grouped bars for each key in a data dict.
+        The data must be a dict with benchmark names as keys. The values should be the bar heights.
+        '''
+        xs = [0]
+        keys = list(data.keys())
+        amount = len(data[keys[0]])
+        colors = self.color_pool[:amount]
+        for heights in data.values():
+            offsets = np.array(range(amount))
+            plot.bar(
+                x=xs[-1] + offsets,
+                height=np.array(heights),
+                width=1,
+                color=colors,
+            )
+
+            # add some bar widths of padding between each group
+            new_x = xs[-1] + amount + 5
+            xs.append(new_x)
+        xs = xs[:-1]
+
+        tick_centering = [len(data[key]) / 2 for key in data.keys()]
+        set_ticks_func = plot.xticks if hasattr(plot, 'xticks') else plot.set_xticks
+        set_ticks_func(
+            ticks=np.array(xs) + np.array(tick_centering),
+            labels=list(data.keys()),
+            rotation=90
+        )
 
     def load_data(self) -> None:
         '''
--- scripts/stats/plotters/sens_analysis/ift_sensitivity.py
+++ scripts/stats/plotters/sens_analysis/ift_sensitivity.py
@@ -13,55 +13,31 @@ class IFTSensitivity(Plotter):
     fname = 'ift_sensitivity'
     description = 'Sensitivity analysis of IPC improvements relative to baseline with various LLL in-flight cycle thresholds'
 
-    log_dir = '/cluster/home/markuswh/gem5-runahead/spec2017/logs'
-
     run_pat = re.compile(r'^m5out\-gem5\-spec2017\-bench\-traditional\-re\-ift\-(\d+)$')
-
-    run_colors = [
-        'green',
-        'red',
-        'darkorange',
-        'blue',
-        'magenta',
-        'darkslategray',
-        'cyan',
-        'tan',
-    ]
 
     def load_data(self) -> None:
         self.data = {}
-        for bench_dir in os.scandir(self.log_dir):
-            if bench_dir.name not in self.valid_benchmarks:
-                continue
-            self.data[bench_dir.name] = {}
-            LOG.debug(f'reading data for benchmark: {bench_dir.name}')
+        for bench in self.benchmarks():
+            self.data[bench] = {}
+            LOG.debug(f'reading data for benchmark: {bench}')
 
             # Read the baseline stats
-            baseline_stats_path = os.path.join(
-                self.log_dir,
-                bench_dir.name,
-                'm5out-gem5-spec2017-bench-baseline-o3',
-                'gem5stats.json'
-            )
             LOG.debug('\t reading baseline stats')
-            with open(baseline_stats_path, 'r') as f:
-                data = json.load(f)
-                self.data[bench_dir.name]['baseline'] = data['sections'][0]['stats']
+            self.data[bench]['baseline'] = self.read_stats(
+                bench,
+                'm5out-gem5-spec2017-bench-baseline-o3'
+            )
 
             # Find all the In-Flight Threshold simulation outputs
-            for run_dir in os.scandir(bench_dir):
-                match = self.run_pat.match(run_dir.name)
+            for run in self.experiments(bench):
+                match = self.run_pat.match(run)
                 if not match:
                     continue
+                ift = match.group(1)
 
                 # Read their stats
-                LOG.debug(f'\t reading stats: {run_dir.name}')
-                stats_path = os.path.join(bench_dir.path, run_dir.name, 'gem5stats.json')
-                with open(stats_path, 'r') as f:
-                    if not bench_dir.name in self.data:
-                        self.data[bench_dir.name] = {}
-                    data = json.load(f)
-                    self.data[bench_dir.name][match.group(1)] = data['sections'][0]['stats']
+                LOG.debug(f'\t reading stats: {run}')
+                self.data[bench][ift] = self.read_stats(bench, run)
 
     def plot(self) -> None:
         benchmarks = [b for b in self.data.keys()]
@@ -73,15 +49,14 @@ class IFTSensitivity(Plotter):
         relative_ipcs = {b: [] for b in benchmarks}
         for bench, data in self.data.items():
             LOG.info(f'computing relative IPCs for {bench}:')
-            base_ipc = data['baseline']['system']['processor']['cores1']['core']['ipc']['values'][0]
+            base_ipc = self.read_stat('system.processor.cores1.core.ipc', data=data['baseline'])
             for run in runs:
-                ipc = data[run]['system']['processor']['cores1']['core']['ipc']['values'][0]
+                ipc = self.read_stat('system.processor.cores1.core.ipc', data=data[run])
                 relative_ipcs[bench].append(ipc / base_ipc)
                 LOG.info(f'\t{run} - {relative_ipcs[bench][-1]}')
 
         # 2nd pass to compute geometric mean IPC increase across all benchmarks
         gmeans = []
-        # runs = ('50', '100', '150', '200', '250')
         LOG.info('computing geometric mean relative IPCs')
         for i, run in enumerate(runs):
             ipcs = []
@@ -91,109 +66,30 @@ class IFTSensitivity(Plotter):
             ipcs = np.array(ipcs)
             gmeans.append(np.power(np.prod(ipcs), 1/len(benchmarks)))
             LOG.info(f'\t{run} - {gmeans[-1]}')
+        relative_ipcs['gmeans'] = gmeans
 
         # plot each benchmark's relative IPCs
-        xs = [0]
-        bar_width = 1
-        offsets = np.array([j * bar_width for j in range(len(runs))])
-        assert(len(offsets) == len(runs))
-        for bench in benchmarks:
-            plt.bar(
-                x=xs[-1] + offsets,
-                height=np.array(relative_ipcs[bench]),
-                width=bar_width,
-                color=self.run_colors,
-            )
-
-            # add some bar widths of padding between each group
-            new_x = xs[-1] + (bar_width * len(runs)) + 5 * bar_width
-            xs.append(new_x)
-
-        # plot geometric means
-        plt.bar(
-            x=xs[-1] + offsets,
-            height=np.array(gmeans),
-            width=bar_width,
-            color=self.run_colors,
-        )
+        self.plot_grouped_bars(relative_ipcs, plt)
 
         plt.ylabel('Relative IPC')
-        plt.xticks(
-            np.array(xs) + (bar_width * len(runs)) / 2,
-            list(benchmarks) + ['gmean'],
-            rotation=90
-        )
         plt.ylim(0.8)
-
         plt.legend(
             runs,
             ncol=2,
-            markerscale=0.5,
-            fontsize=8,
-            handleheight=0.7,
-            handlelength=0.7,
+            markerscale=0.5, fontsize=8,
+            handleheight=0.7, handlelength=0.7,
             loc='lower left'
         )
         leg = plt.gca().get_legend()
         for i, handle in enumerate(leg.legendHandles):
-            handle.set_color(self.run_colors[i])
+            handle.set_color(self.color_pool[i])
 
-        plt.axhline(y=1.0, color='black', linewidth=0.5, linestyle='--')
+        plt.axhline(y=1.0, color='black', linewidth=0.5, linestyle='--', label='_nolegend_')
 
-class IFTSensitivityReal(Plotter):
+class IFTSensitivityReal(IFTSensitivity):
     name = 'NIPC relative to baseline for runahead IFTs'
     fname = 'ift_sensitivity_real'
     description = 'Sensitivity analysis of IPC improvements relative to baseline with various LLL in-flight cycle thresholds'
-
-    log_dir = '/cluster/home/markuswh/gem5-runahead/spec2017/logs'
-
-    run_pat = re.compile(r'^m5out\-gem5\-spec2017\-bench\-traditional\-re\-ift\-(\d+)$')
-
-    run_colors = [
-        'green',
-        'red',
-        'darkorange',
-        'blue',
-        'magenta',
-        'darkslategray',
-        'cyan',
-        'tan',
-    ]
-
-    def load_data(self) -> None:
-        self.data = {}
-        for bench_dir in os.scandir(self.log_dir):
-            if bench_dir.name not in self.valid_benchmarks:
-                continue
-            self.data[bench_dir.name] = {}
-            LOG.debug(f'reading data for benchmark: {bench_dir.name}')
-
-            # Read the baseline stats
-            baseline_stats_path = os.path.join(
-                self.log_dir,
-                bench_dir.name,
-                'm5out-gem5-spec2017-bench-baseline-o3',
-                'gem5stats.json'
-            )
-            LOG.debug('\t reading baseline stats')
-            with open(baseline_stats_path, 'r') as f:
-                data = json.load(f)
-                self.data[bench_dir.name]['baseline'] = data['sections'][0]['stats']
-
-            # Find all the In-Flight Threshold simulation outputs
-            for run_dir in os.scandir(bench_dir):
-                match = self.run_pat.match(run_dir.name)
-                if not match:
-                    continue
-
-                # Read their stats
-                LOG.debug(f'\t reading stats: {run_dir.name}')
-                stats_path = os.path.join(bench_dir.path, run_dir.name, 'gem5stats.json')
-                with open(stats_path, 'r') as f:
-                    if not bench_dir.name in self.data:
-                        self.data[bench_dir.name] = {}
-                    data = json.load(f)
-                    self.data[bench_dir.name][match.group(1)] = data['sections'][0]['stats']
 
     def plot(self) -> None:
         benchmarks = [b for b in self.data.keys()]
@@ -207,15 +103,14 @@ class IFTSensitivityReal(Plotter):
         relative_ipcs = {b: [] for b in benchmarks}
         for bench, data in self.data.items():
             LOG.info(f'computing relative IPCs for {bench}:')
-            base_ipc = data['baseline']['system']['processor']['cores1']['core']['realIpc']['values'][0]
+            base_ipc = self.read_stat('system.processor.cores1.core.realIpc', data=data['baseline'])
             for run in runs:
-                ipc = data[run]['system']['processor']['cores1']['core']['realIpc']['values'][0]
+                ipc = self.read_stat('system.processor.cores1.core.realIpc', data=data[run])
                 relative_ipcs[bench].append(ipc / base_ipc)
                 LOG.info(f'\t{run} - {relative_ipcs[bench][-1]}')
 
         # 2nd pass to compute geometric mean IPC increase across all benchmarks
         gmeans = []
-        # runs = ('50', '100', '150', '200', '250')
         LOG.info('computing geometric mean relative IPCs')
         for i, run in enumerate(runs):
             ipcs = []
@@ -225,60 +120,30 @@ class IFTSensitivityReal(Plotter):
             ipcs = np.array(ipcs)
             gmeans.append(np.power(np.prod(ipcs), 1/len(benchmarks)))
             LOG.info(f'\t{run} - {gmeans[-1]}')
+        relative_ipcs['gmean'] = gmeans
 
         # plot each benchmark's relative IPCs
-        xs = [0]
-        bar_width = 1
-        offsets = np.array([j * bar_width for j in range(len(runs))])
-        assert(len(offsets) == len(runs))
-        for bench in benchmarks:
-            ax1.bar(
-                x=xs[-1] + offsets,
-                height=np.array(relative_ipcs[bench]),
-                width=bar_width,
-                color=self.run_colors,
-            )
-
-            # add some bar widths of padding between each group
-            new_x = xs[-1] + (bar_width * len(runs)) + 5 * bar_width
-            xs.append(new_x)
-
-        # plot geometric means
-        ax1.bar(
-            x=xs[-1] + offsets,
-            height=np.array(gmeans),
-            width=bar_width,
-            color=self.run_colors,
-        )
+        self.plot_grouped_bars(relative_ipcs, ax1)
+        ax1.axhline(y=1.0, color='black', linewidth=0.5, linestyle='--', label='_nolegend_')
 
         ax1.set_ylabel('Relative NIPC')
-        ax1.set_xticks(
-            np.array(xs) + (bar_width * len(runs)) / 2,
-            list(benchmarks) + ['gmean'],
-            rotation=90
-        )
         ax1.set_ylim(0.9)
-
         leg = fig.legend(
             runs,
             ncol=2,
-            markerscale=0.5,
-            fontsize=8,
-            handleheight=0.7,
-            handlelength=0.7,
+            markerscale=0.5, fontsize=8,
+            handleheight=0.7, handlelength=0.7,
             bbox_to_anchor=(0.19, 0.86)
         )
         for i, handle in enumerate(leg.legendHandles):
-            handle.set_color(self.run_colors[i])
-
-        ax1.axhline(y=1.0, color='black', linewidth=0.5, linestyle='--')
+            handle.set_color(self.color_pool[i])
 
         # Read pseudoretired insts
         pseudoretireds = {b: [] for b in benchmarks}
         for bench, data in self.data.items():
             LOG.info(f'reading pseudoretired insts for {bench}:')
             for run in runs:
-                pr = data[run]['system']['processor']['cores1']['core']['pseudoRetiredInsts']['values'][0]
+                pr = self.read_stat('system.processor.cores1.core.pseudoRetiredInsts', data=data[run])
                 pseudoretireds[bench].append(pr)
                 LOG.info(f'\t{run} - {pseudoretireds[bench][-1]}')
 
@@ -293,96 +158,20 @@ class IFTSensitivityReal(Plotter):
             prs = np.array(prs)
             means.append(prs.mean())
             LOG.info(f'\t{run} - {means[-1]}')
+        pseudoretireds['mean'] = means
 
-        xs = [0]
-        bar_width = 1
-        offsets = np.array([j * bar_width for j in range(len(runs))])
-        assert(len(offsets) == len(runs))
-        for bench in benchmarks:
-            ax2.bar(
-                x=xs[-1] + offsets,
-                height=np.array(pseudoretireds[bench]),
-                width=bar_width,
-                color=self.run_colors,
-            )
-
-            # add some bar widths of padding between each group
-            new_x = xs[-1] + (bar_width * len(runs)) + 5 * bar_width
-            xs.append(new_x)
-
-        # plot means
-        ax2.bar(
-            x=xs[-1] + offsets,
-            height=np.array(means),
-            width=bar_width,
-            color=self.run_colors,
-        )
+        self.plot_grouped_bars(pseudoretireds, ax2)
 
         ax2.set_title('Insts pseudoretired')
         ax2.set_ylabel('Instructions')
-        ax2.set_xticks(
-            np.array(xs) + (bar_width * len(runs)) / 2,
-            list(benchmarks) + ['mean'],
-            rotation=90
-        )
         ax2.set_yscale('log')
 
         fig.set_figwidth(10)
 
-class OverheadAdjustedIFTSensitivity(Plotter):
+class OverheadAdjustedIFTSensitivity(IFTSensitivity):
     name = 'Overhead-adjusted IPC relative to baseline for runahead IFTs'
     fname = 'ift_sensitivity_overhead_adjusted'
     description = 'Sensitivity analysis of IPC improvements relative to baseline with various LLL in-flight cycle thresholds'
-
-    log_dir = '/cluster/home/markuswh/gem5-runahead/spec2017/logs'
-
-    run_pat = re.compile(r'^m5out\-gem5\-spec2017\-bench\-traditional\-re\-ift\-(\d+)$')
-
-    run_colors = [
-        'green',
-        'red',
-        'darkorange',
-        'blue',
-        'magenta',
-        'darkslategray',
-        'cyan',
-        'tan',
-    ]
-
-    def load_data(self) -> None:
-        self.data = {}
-        for bench_dir in os.scandir(self.log_dir):
-            if bench_dir.name not in self.valid_benchmarks:
-                continue
-            self.data[bench_dir.name] = {}
-            LOG.debug(f'reading data for benchmark: {bench_dir.name}')
-
-            # Read the baseline stats
-            baseline_stats_path = os.path.join(
-                self.log_dir,
-                bench_dir.name,
-                'm5out-gem5-spec2017-bench-baseline-o3',
-                'gem5stats.json'
-            )
-            LOG.debug('\t reading baseline stats')
-            with open(baseline_stats_path, 'r') as f:
-                data = json.load(f)
-                self.data[bench_dir.name]['baseline'] = data['sections'][0]['stats']
-
-            # Find all the In-Flight Threshold simulation outputs
-            for run_dir in os.scandir(bench_dir):
-                match = self.run_pat.match(run_dir.name)
-                if not match:
-                    continue
-
-                # Read their stats
-                LOG.debug(f'\t reading stats: {run_dir.name}')
-                stats_path = os.path.join(bench_dir.path, run_dir.name, 'gem5stats.json')
-                with open(stats_path, 'r') as f:
-                    if not bench_dir.name in self.data:
-                        self.data[bench_dir.name] = {}
-                    data = json.load(f)
-                    self.data[bench_dir.name][match.group(1)] = data['sections'][0]['stats']
 
     def plot(self) -> None:
         benchmarks = [b for b in self.data.keys()]
@@ -394,11 +183,11 @@ class OverheadAdjustedIFTSensitivity(Plotter):
         relative_ipcs = {b: [] for b in benchmarks}
         for bench, data in self.data.items():
             LOG.info(f'computing relative IPCs for {bench}:')
-            base_ipc = data['baseline']['system']['processor']['cores1']['core']['ipc']['values'][0]
+            base_ipc = self.read_stat('system.processor.cores1.core.ipc', data=data['baseline'])
             for run in runs:
-                insts = data[run]['simInsts']['values'][0]
-                adjusted_cycles = data[run]['system']['processor']['cores1']['core']['numCycles']['values'][0]
-                adjusted_cycles -= data[run]['system']['processor']['cores1']['core']['commit']['totalRunaheadOverhead']['values'][0]
+                insts = self.read_stat('simInsts', data=data[run])
+                adjusted_cycles = self.read_stat('system.processor.cores1.core.numCycles', data=data[run])
+                adjusted_cycles -= self.read_stat('system.processor.cores1.core.commit.totalRunaheadOverhead', data=data[run])
                 adjusted_ipc = insts / adjusted_cycles
                 relative_ipcs[bench].append(adjusted_ipc / base_ipc)
                 LOG.info(f'\t{run} - {relative_ipcs[bench][-1]}')
@@ -414,51 +203,22 @@ class OverheadAdjustedIFTSensitivity(Plotter):
             ipcs = np.array(ipcs)
             gmeans.append(np.power(np.prod(ipcs), 1/len(benchmarks)))
             LOG.info(f'\t{run} - {gmeans[-1]}')
+        relative_ipcs['gmean'] = gmeans
 
-        # plot each benchmark's relative IPCs
-        xs = [0]
-        bar_width = 1
-        offsets = np.array([j * bar_width for j in range(len(runs))])
-        assert(len(offsets) == len(runs))
-        for bench in benchmarks:
-            plt.bar(
-                x=xs[-1] + offsets,
-                height=np.array(relative_ipcs[bench]),
-                width=bar_width,
-                color=self.run_colors,
-            )
+        self.plot_grouped_bars(relative_ipcs, plt)
+        plt.axhline(y=1.0, color='black', linewidth=0.5, linestyle='--', label='_nolegend_')
 
-            # add some bar widths of padding between each group
-            new_x = xs[-1] + (bar_width * len(runs)) + 5 * bar_width
-            xs.append(new_x)
-
-        # plot geometric means
-        plt.bar(
-            x=xs[-1] + offsets,
-            height=np.array(gmeans),
-            width=bar_width,
-            color=self.run_colors,
-        )
-
+        # labels, ticks, etc.
         plt.ylabel('Relative IPC')
-        plt.xticks(
-            np.array(xs) + (bar_width * len(runs)) / 2,
-            list(benchmarks) + ['gmean'],
-            rotation=90
-        )
         plt.ylim(0.9)
 
         plt.legend(
             runs,
             ncol=2,
-            markerscale=0.5,
-            fontsize=8,
-            handleheight=0.7,
-            handlelength=0.7,
+            markerscale=0.5, fontsize=8,
+            handleheight=0.7, handlelength=0.7,
             loc='lower left'
         )
         leg = plt.gca().get_legend()
         for i, handle in enumerate(leg.legendHandles):
-            handle.set_color(self.run_colors[i])
-
-        plt.axhline(y=1.0, color='black', linewidth=0.5, linestyle='--')
+            handle.set_color(self.color_pool[i])
